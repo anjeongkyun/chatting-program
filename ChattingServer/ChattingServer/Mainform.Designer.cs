a407b1e80dff828325098c85f9ce224cdf4e9b9f
Anjeongkyun <97106584+Anjeongkyun@users.noreply.github.com>
2022-02-23 15:46:23 +0900

    jkan commit
    chatting server first commit

---
+++ ChattingServer/ChattingServer/Mainform.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ChattingServer
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb_text = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_data_send = new System.Windows.Forms.Button();
+            this.btn_data_send_all = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtb_send_Text = new System.Windows.Forms.RichTextBox();
+            this.lb_client_list = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // rtb_text
+            // 
+            this.rtb_text.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtb_text.Location = new System.Drawing.Point(276, 36);
+            this.rtb_text.Name = "rtb_text";
+            this.rtb_text.ReadOnly = true;
+            this.rtb_text.Size = new System.Drawing.Size(391, 398);
+            this.rtb_text.TabIndex = 1;
+            this.rtb_text.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "채팅 내용";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "접속한 클라이언트 리스트";
+            // 
+            // btn_data_send
+            // 
+            this.btn_data_send.Location = new System.Drawing.Point(40, 401);
+            this.btn_data_send.Name = "btn_data_send";
+            this.btn_data_send.Size = new System.Drawing.Size(94, 33);
+            this.btn_data_send.TabIndex = 5;
+            this.btn_data_send.Text = "보내기";
+            this.btn_data_send.UseVisualStyleBackColor = true;
+            this.btn_data_send.Click += new System.EventHandler(this.btn_data_send_Click);
+            // 
+            // btn_data_send_all
+            // 
+            this.btn_data_send_all.Location = new System.Drawing.Point(144, 401);
+            this.btn_data_send_all.Name = "btn_data_send_all";
+            this.btn_data_send_all.Size = new System.Drawing.Size(94, 33);
+            this.btn_data_send_all.TabIndex = 6;
+            this.btn_data_send_all.Text = "전체 보내기";
+            this.btn_data_send_all.UseVisualStyleBackColor = true;
+            this.btn_data_send_all.Click += new System.EventHandler(this.btn_data_send_all_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "보낼 내용";
+            // 
+            // rtb_send_Text
+            // 
+            this.rtb_send_Text.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtb_send_Text.Location = new System.Drawing.Point(12, 288);
+            this.rtb_send_Text.Name = "rtb_send_Text";
+            this.rtb_send_Text.Size = new System.Drawing.Size(226, 107);
+            this.rtb_send_Text.TabIndex = 10;
+            this.rtb_send_Text.Text = "";
+            // 
+            // lb_client_list
+            // 
+            this.lb_client_list.FormattingEnabled = true;
+            this.lb_client_list.ItemHeight = 12;
+            this.lb_client_list.Location = new System.Drawing.Point(14, 47);
+            this.lb_client_list.Name = "lb_client_list";
+            this.lb_client_list.Size = new System.Drawing.Size(224, 208);
+            this.lb_client_list.TabIndex = 11;
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 450);
+            this.Controls.Add(this.lb_client_list);
+            this.Controls.Add(this.rtb_send_Text);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_data_send_all);
+            this.Controls.Add(this.btn_data_send);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtb_text);
+            this.Name = "Mainform";
+            this.Text = "채팅서버_출처 : https://jeongkyun-it.tistory.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_data_send;
+        private System.Windows.Forms.Button btn_data_send_all;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtb_send_Text;
+        private System.Windows.Forms.ListBox lb_client_list;
+    }
+}
+
---
+++ ChattingServer/ChattingServer/Mainform.cs
@@ -0,0 +1,283 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChattingServer
+{
+    public partial class Mainform : Form
+    {
+        Dictionary<string, Socket> dic_client = null;
+        List<Socket> listSock = null;
+        List<String> listUser = null;
+        Crypto crypto = null;
+        public Mainform()
+        {
+            InitializeComponent();
+            init();
+            serverOpen();
+        }
+
+        /// <summary>
+        /// 변수 초기화
+        /// </summary>
+        private void init()
+        {
+            dic_client = new Dictionary<string, Socket>();
+            listSock = new List<Socket>();
+            listUser = new List<String>();
+            crypto = new Crypto();
+        }
+
+        /// <summary>
+        /// 서버 Open
+        /// </summary>
+        private void serverOpen()
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(IPAddress.Any, 12000));
+            server.Listen(10);
+
+            SocketAsyncEventArgs sockAsync = new SocketAsyncEventArgs();
+            sockAsync.Completed += new EventHandler<SocketAsyncEventArgs>(sockAsync_Completed);
+            server.AcceptAsync(sockAsync);
+        }
+
+        /// <summary>
+        /// 연결 될 시 발생되는 이벤트
+        /// </summary>
+        /// <param name="sender">이벤트 발생시킨 소켓 </param>
+        /// <param name="e"></param>
+        private void sockAsync_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            try
+            {
+
+                Socket server = (Socket)sender;
+                Socket client = e.AcceptSocket;
+                byte[] name = new byte[100];
+                client.Receive(name);
+
+                String str_data = Encoding.UTF8.GetString(name).Trim().Replace("\0", "");
+
+                str_data = crypto.AESDecrypt256(str_data, "1234567890123456");
+
+                JObject jobj = JObject.Parse(str_data);
+
+                string user = jobj["text"].ToString();
+
+                dic_client.Add(user, client);
+                SocketAsyncEventArgs receiveAsync = new SocketAsyncEventArgs();
+                receiveAsync.Completed += new EventHandler<SocketAsyncEventArgs>(receiveAsync_Completed);
+                receiveAsync.SetBuffer(new byte[4096], 0, 4096);
+                receiveAsync.UserToken = client;
+                client.ReceiveAsync(receiveAsync);
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("*********** " + user + "- Connected ***********");
+                sb.AppendLine();
+
+                Invoke((MethodInvoker)delegate
+                {
+                    rtb_text.AppendText(sb.ToString());
+                    lb_client_list.Items.Add(user);
+                });
+
+                e.AcceptSocket = null;
+                server.AcceptAsync(e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 응답이 올 경우 발생하는 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void receiveAsync_Completed(object sender, SocketAsyncEventArgs e)
+        {
+
+            Socket client = (Socket)sender;
+            String Name = string.Empty;
+
+            if (client.Connected && e.BytesTransferred > 0)
+            {
+                int length = e.Buffer.Length;
+
+                //byte[] lengthByte = e.Buffer;
+                //int length = BitConverter.ToInt32(lengthByte, 0);
+                //byte[] data = new byte[length];
+
+                byte[] data = new byte[length];
+
+                client.Receive(data, length, SocketFlags.None);
+                String str_data = Encoding.UTF8.GetString(data).Replace("\0", "");
+
+                if (str_data == "")
+                    return;
+
+                str_data = crypto.AESDecrypt256(str_data, "1234567890123456");
+                JObject jobj = JObject.Parse(str_data);
+
+                if (searchSocket(client) == "")
+                {
+                    MessageBox.Show("접속 클라이언트를 찾을 수 없습니다.");
+                    return;
+                }
+                else
+                    Name = searchSocket(client);
+
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("*********** " + Name + "- receive ***********");
+                sb.AppendLine(jobj["text"].ToString());
+
+                Invoke((MethodInvoker)delegate
+                {
+                    rtb_text.AppendText(sb.ToString());
+                });
+
+                client.ReceiveAsync(e);
+
+            }
+        }
+
+        private String searchSocket(Socket sender)
+        {
+            foreach (String key in dic_client.Keys)
+            {
+                if (dic_client[key] == sender)
+                {
+                    return key;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private void Send(string text, string flag)
+        {
+
+
+
+            int sel_idx = lb_client_list.SelectedIndex;
+
+            if (sel_idx == -1)
+                sel_idx = 0;
+
+            String sender = lb_client_list.Items[sel_idx].ToString();
+            String message = text;
+
+            //전체보내기
+            if (flag == "all")
+            {
+                try
+                {
+                    for (int i = 0; i < lb_client_list.Items.Count; i++)
+                    {
+                        listUser.Add(lb_client_list.Items[i].ToString());
+                    }
+
+                    for (int i = 0; i < listUser.Count; i++)
+                    {
+                        listSock.Add(dic_client[listUser[i]]);
+                    }
+
+                    for (int i = 0; i < listSock.Count; i++)
+                    {
+                        byte[] data = Encoding.UTF8.GetBytes(message);
+                        listSock[i].Send(BitConverter.GetBytes(data.Length));
+                        listSock[i].Send(data, data.Length, SocketFlags.None);
+                    }
+                    listSock.Clear();
+                    listUser.Clear();
+                }
+                catch (Exception ex) { MessageBox.Show("Send Error : " + ex.Message); }
+            }
+            //특정 클라이언트에게 보내기
+            else
+            {
+                try
+                {
+                    if (dic_client.ContainsKey(sender))
+                    {
+                        Socket client = dic_client[sender];
+                        byte[] data = Encoding.UTF8.GetBytes(message);
+                        client.Send(BitConverter.GetBytes(data.Length));
+                        client.Send(data, data.Length, SocketFlags.None);
+                    }
+                    else
+                    {
+                        MessageBox.Show("소켓을 찾지 못했습니다");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Send Error : " + ex.Message);
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// 특정 클라이언트에게 메세지 보내기
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_data_send_Click(object sender, EventArgs e)
+        {
+            if (lb_client_list.Items.Count == 0)
+            {
+                MessageBox.Show("보낼 클라이언트가 없습니다.");
+                return;
+            }
+
+            if (rtb_send_Text.Text == "")
+            {
+                MessageBox.Show("보낼 메세지를 입력해주세요.");
+                return;
+            }
+
+            string msg = crypto.AESEncrypt256(rtb_send_Text.Text, "1234567890123456");
+
+            Send(msg, "one");
+        }
+
+        /// <summary>
+        /// 모든 클라이언트에게 보내기
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_data_send_all_Click(object sender, EventArgs e)
+        {
+            if (lb_client_list.Items.Count == 0)
+            {
+                MessageBox.Show("보낼 클라이언트가 없습니다.");
+                return;
+            }
+
+            if (rtb_send_Text.Text == "")
+            {
+                MessageBox.Show("보낼 메세지를 입력해주세요.");
+                return;
+            }
+
+            string msg = crypto.AESEncrypt256(rtb_send_Text.Text, "1234567890123456");
+
+            Send(msg, "all");
+        }
+    }
+}
